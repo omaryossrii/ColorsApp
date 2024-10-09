4bebfbc824a5d52c3df30a82ced4285975d23b3d
omaryossrii <omar.yossri@oneorder.net>
2024-10-09 10:47:50 +0300

    Initial Commit

---
+++ RandomColorsTests/RandomColorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColorsTests.swift
+//  RandomColorsTests
+//
+//  Created by Omar Yossri on 09/10/2024.
+//
+
+import Testing
+@testable import RandomColors
+
+struct RandomColorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
